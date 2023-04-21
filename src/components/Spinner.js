1e6353174b487034841ca2ec10198c37ce384f88
rt885 <ruben.t.v@hotmail.com>
2023-04-21 08:43:21 -0600

    implementacion de admin-page

---
+++ src/components/Spinner.js
@@ -0,0 +1,11 @@
+import "./Spinner.css";
+
+function Spinner() {
+  return (
+    <div className="Spinner">
+      <div className="Spinner__animation"></div>
+    </div>
+  );
+}
+
+export default Spinner;
