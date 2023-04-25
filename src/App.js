a2f8b9730fab92e08d0b5b2549b6c955e1ce9b2d
rt885 <ruben.t.v@hotmail.com>
2023-04-24 18:14:46 -0600

    desarrollo de login para admin, responsive login y signup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './pages/Login';
 import Landing from './Landing';
 import Home from './pages/Home';
 import Admin_Page from './pages/Admin_Page';
+import Login_Admin from './pages/Login_Admin';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/home" element={<Home />} />
         <Route path="/admin_page" element={<Admin_Page />} />
+        <Route path="/login_admin" element={<Login_Admin />} />
         <Route path="/" element={
             <>
               <Landing />
