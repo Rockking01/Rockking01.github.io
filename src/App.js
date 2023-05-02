4c243cc405b3c4e83531ac06444450212cb7ce30
rt885 <ruben.t.v@hotmail.com>
2023-05-02 00:08:22 -0600

    dashboard admin terminado, navbar creado

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import Signup from "./pages/Signup";
 import LoginView from "./pages/Login2";
 import Landing from "./Landing";
-import Home from "./pages/Home";
 import AdminPage from "./pages/AdminPage";
 import ProfileView from "./pages/ProfileView";
-import ChartData from "./pages/ChartData";
 
 
 function App() {
@@ -13,9 +11,8 @@ function App() {
     <div className="">
       <Routes>
         <Route path="/login" element={<LoginView />} />
+        <Route path="/landing" element={<Landing />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/chart_data" element={<ChartData />} />
         <Route path="/admin_page" element={<AdminPage />} />
         <Route path="/profile/:id" element={<ProfileView />} />
         <Route
---
+++ src/components/NavbarAdmin.js
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import './Navbar.css'
+import logo from '../assets/logo.png'
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  );
+}
+
+const NavbarAdmin = () => {
+  const [showNavbar, setShowNavbar] = useState(false)
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar)
+  }
+
+  return (
+    <nav className="navbar">
+      <div className="container-nav">
+        <img className="logo-nav" src={logo} alt="Logo de mi aplicación" />
+        <div className="menu-icon" onClick={handleShowNavbar}>
+          <svg viewBox="0 0 100 80" width="40" height="40">
+            <rect width="100" height="20"></rect>
+            <rect y="30" width="100" height="20"></rect>
+            <rect y="60" width="100" height="20"></rect>
+          </svg>
+        </div>
+        <div className={`nav-elements  ${showNavbar && 'active'}`}>
+          <ul>
+            <li>
+              <CustomLink to="/landing">
+                <p>Inicio</p>
+              </CustomLink>
+            </li>
+            <li>
+              <CustomLink to="/admin_page">
+                <p>Videojuego</p>
+              </CustomLink>
+            </li>
+            <li>
+              <CustomLink to="/admin_page">
+                <p>Cerrar Sesion</p>
+              </CustomLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default NavbarAdmin
